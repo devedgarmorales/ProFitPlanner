33968ce5100e33d69eaa14ead8cbb6b099badee7
Kevin López González <111819179+KevinLG357@users.noreply.github.com>
2025-01-06 16:34:27 -0600

    librerias para hacer test

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'react-native-reanimated/plugin', 
-  ],
+presets: [
+    'module:metro-react-native-babel-preset',
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+    [
+    '@babel/preset-env',
+    {
+        targets: {
+        node: 'current',
+        },
+    },
+    ],
+],
+plugins: [
+    'react-native-reanimated/plugin',
+    '@babel/plugin-transform-private-methods',
+    '@babel/plugin-transform-class-properties',
+    '@babel/plugin-transform-private-property-in-object',
+],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,35 @@
 module.exports = {
-  preset: 'react-native',
-};
+preset: 'react-native',
+setupFiles: ['<rootDir>/node_modules/react-native-gesture-handler/jestSetup.js'],
+setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+},
+transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|react-navigation|@react-native-community|@expo/vector-icons|react-clone-referenced-element|@react-native-picker|react-native-gesture-handler|@react-navigation/elements)',
+],
+moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+},
+moduleDirectories: ['node_modules', 'src'],
+testEnvironment: 'node',
+testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/android/',
+    '/ios/',
+    '/__tests__/',
+    '/src/types/',
+],
+globals: {
+    'ts-jest': {
+    tsconfig: 'tsconfig.json',
+    },
+},
+automock: false,
+resetMocks: true,
+clearMocks: true,
+}
